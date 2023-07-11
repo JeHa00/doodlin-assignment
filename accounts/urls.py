799f0ef97cc154ffda7d33860ccdb8f276fe3cc7
jeha00 <rudtls0611@naver.com>
2023-07-11 20:06:41 +0900

    FEAT: update url configure

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path("signup/", views.SignUpView.as_view(), name="signup"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("guide/", login_required(views.guide_view), name="guide"),
+    path("accounts/signup/", views.SignUpView.as_view(), name="signup"),
+    path("accounts/login/", views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", views.logout_view, name="logout"),
+    path("accounts/guide/", views.guide_view, name="guide"),
+    path("accounts/signup-list/", views.SignupListView.as_view(), name="signup_list"),
+    path("employees/", views.EmployeeListView.as_view(), name="employee_list"),
 ]
